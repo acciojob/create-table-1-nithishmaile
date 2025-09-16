2a919fb41ee024397c4dd5ff0d294e027ed570eb
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-16 17:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const tableElement=document.getElementById("sampleTable");
+/*const tableElement=document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code
             const trElement=document.createElement("tr");
@@ -12,4 +12,14 @@ function insert_Row() {
   
   
   
+}*/
+function insert_Row() {
+    const tableBody = document.querySelector("#sampleTable tbody");
+    const trElement = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    td1.textContent = "New Cell1";
+    td2.textContent = "New Cell2";
+    trElement.append(td1, td2);
+    tableBody.insertBefore(trElement, tableBody.firstChild);
 }
